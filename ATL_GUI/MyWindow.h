ef7219aa9a679f94351b4fb753e38a02f8554695
SOHU-INC\qiangyin <qiangyin@sohu-inc.com>
2019-03-06 11:43:03 +0800

    高级消息映射和嵌入类

--- ATL_GUI/MyWindow.h
+++ ATL_GUI/MyWindow.h
@@ -2,17 +2,18 @@
 #include "stdafx.h"
 #include "PaintBkgnd.h"
 
-class CMyWindow : public CWindowImpl<CMyWindow, CWindow, CFrameWinTraits>
+class CMyWindow : public CWindowImpl<CMyWindow, CWindow, CFrameWinTraits>, public CPaintBkgnd<CMyWindow, RGB(0, 0, 255)>
 {
 public:
 	DECLARE_WND_CLASS(_T("My Window Class"));
 
-	//typedef CPaintBkgnd<CMyWindow, RGB(0, 0, 255)> CPaintBkgndBase;
+	typedef CPaintBkgnd<CMyWindow, RGB(0, 0, 255)> CPaintBkgndBase;
 
 	BEGIN_MSG_MAP(CMyWindow)
 		MESSAGE_HANDLER(WM_CLOSE, OnClose)
 		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
 		//COMMAND_ID_HANDLER(IDC_ABOUT, OnAbout)
+		CHAIN_MSG_MAP(CPaintBkgndBase)
 	END_MSG_MAP()
 	
 	LRESULT OnClose(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled);
